06becf998db0099a7fa1168243e5326d070775bd
Philipp Gaßner <activator112233@gmail.com>
2024-08-26 23:58:50 +0200

    refactored for github

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"tuiplayground"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/thallosaurus/go-git-tk/pkg/views"
 )
 
 func main() {
-	initialModel := tuiplayground.NewCliModel()
+	initialModel := views.NewCliModel()
 
 	p := tea.NewProgram(initialModel)
 
